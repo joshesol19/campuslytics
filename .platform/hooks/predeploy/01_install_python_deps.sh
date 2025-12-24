6fcbe366506d35275f333ab4db7cc428f468d169
Joshesol <joshuaericsolano@gmail.com>
2025-12-24 01:09:48 -0800

    Install python deps during EB deploy

--- .platform/hooks/predeploy/01_install_python_deps.sh
+++ .platform/hooks/predeploy/01_install_python_deps.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
-# install python + pip (AL2023 / modern EB images usually have dnf)
 if command -v dnf >/dev/null 2>&1; then
-  dnf -y install python3 python3-pip
+  dnf -y install python3
 else
-  yum -y install python3 python3-pip
+  yum -y install python3
 fi
 
-# DON'T upgrade pip (it's rpm-managed and will fail)
-python3 -m pip install -r requirements.txt --no-cache-dir
+# bootstrap pip if it's missing
+python3 -m ensurepip --upgrade || true
+
+# now install deps
+python3 -m pip install --no-cache-dir -r requirements.txt
